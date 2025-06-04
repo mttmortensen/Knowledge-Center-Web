4969b039fb38ca956d43557bfa21b897f41f1acc
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-03 19:27:06 -0600

    domains now made to match be model

---
+++ js/types/domains.ts
@@ -0,0 +1,9 @@
+export interface Domains 
+{
+    DomainId: number;
+    DomainName: string;
+    DomainDescription: string;
+    DomainStatus: string;
+    CreatedAt: string;
+    LastUsed: string;
+}
