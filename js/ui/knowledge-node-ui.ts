d417c9c02e876f59a5d2663474238c92d82aaf64
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-04 09:17:14 -0600

    starting on logic for getting kns listed out

--- js/ui/knowledge-node-ui.ts
+++ js/ui/knowledge-node-ui.ts
@@ -2,6 +2,7 @@ import { getAllKnowledgeNodes } from "../services/knowledge-node-service.js";
 
 document.addEventListener("DOMContentLoaded", () => 
 {
+    // === Main Menu Button Logic ===
     const viewBtn = document.getElementById("view-nodes");
 
     if (viewBtn) 
@@ -11,5 +12,12 @@ document.addEventListener("DOMContentLoaded", () =>
             const nodes = await getAllKnowledgeNodes();
             console.log("Knowledge Nodes:", nodes)
         })
+    }
+
+    // === KN List Page Logic ===
+    const nodeListContainer = document.getElementById("node-list");
+    if(nodeListContainer) 
+    {
+        loadAndRenderNodeList(nodeListContainer);
     }
 })
