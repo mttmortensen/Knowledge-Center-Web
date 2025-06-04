807587ff144092810ca9a5cef69ce531647ba2d8
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-03 19:23:39 -0600

    matches backend model

---
+++ js/types/knowledge-node.ts
@@ -0,0 +1,12 @@
+export interface KnowledgeNode 
+{
+    Id: number;
+    Title: string;
+    DomainId: number;
+    NodeType: string;
+    Description: string;
+    ConfidenceLevel: number;
+    Status: string
+    CreatedAt: string;
+    LastUpdated: string;
+}
