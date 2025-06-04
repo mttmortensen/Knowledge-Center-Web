59d7aab2739bcb29ad11532126f80829fe1d591e
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-03 19:25:45 -0600

    removing previous test code for main

--- js/main.ts
+++ js/main.ts
@@ -1,13 +1,1 @@
-class KnowledgeNode {
-  constructor(
-    public title: string,
-    public domain: string
-  ) {}
 
-  display(): void {
-    console.log(`📘 ${this.title} (${this.domain})`);
-  }
-}
-
-const node = new KnowledgeNode("DNS Lookup", "Networking");
-node.display();
