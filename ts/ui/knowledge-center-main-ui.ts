5188593a6c7cb63970e188efe1a6dc5a31a5f942
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-04 14:44:12 -0600

    having domain button get to domain main html

--- ts/ui/knowledge-center-main-ui.ts
+++ ts/ui/knowledge-center-main-ui.ts
@@ -1,3 +1,7 @@
   document.getElementById("menu-nodes")?.addEventListener("click", () => {
     window.location.href = "/kn/knowledge-nodes-main.html";
   });
+
+    document.getElementById("menu-domains")?.addEventListener("click", () => {
+    window.location.href = "/domains/domains-main.html";
+  });
