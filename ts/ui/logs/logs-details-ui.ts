25a92088a0ce652b40072a7e07ab8a5e8087866f
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-04 21:40:12 -0600

    updated naming for file. us to ui

---
+++ ts/ui/logs/logs-details-ui.ts
@@ -0,0 +1,56 @@
+import { getALogById } from "../../services/log-services.js";
+import { LogEntry } from "../../types/log-entry.js";
+
+document.addEventListener("DOMContentLoaded", async () => 
+{
+    const logId = getLogIdFromUrl();
+
+    if (logId == null)
+    {
+        displayError("No Knowledge Node ID provided in URL.");
+        return;
+    }
+
+    const log = await getALogById(logId);
+
+    if(!log) 
+    {
+        displayError(`Could not load Knowledge Node with ID ${logId}.`);
+        return;
+    }
+
+    renderLogDetails(log);
+
+    // === Back button ===
+    const backBtn = document.getElementById("back-button");
+    if(backBtn)
+    {
+        backBtn.addEventListener("click", () => 
+        {
+             window.history.back();
+        });
+    }
+});
+
+function getLogIdFromUrl(): number | null 
+{
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+    return id ? parseInt(id) : null;
+}
+
+function displayError(message: string) 
+{
+    const container = document.getElementById("kn-details");
+    if (container)
+    {
+        container.innerHTML = `<p style="color:red;">${message}<\p>`;
+    }
+}
+
+function renderLogDetails(log: LogEntry)
+{
+    (document.getElementById("log-content") as HTMLElement).textContent = log.Content;
+    (document.getElementById("log-entry-date") as HTMLElement).textContent = log.EntryDate;
+    (document.getElementById("log-progress") as HTMLElement).textContent = log.ContributesToProgress.toString();
+}
