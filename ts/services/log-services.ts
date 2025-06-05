57ad6e1ebfb823c0cb987d48079d90cb92b5714a
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-04 21:21:51 -0600

    log service file for getting log(s)

---
+++ ts/services/log-services.ts
@@ -0,0 +1,43 @@
+import { LogEntry } from "../types/log-entry";
+
+const API_BASE_URL = "http://localhost:8080/api/logs";
+
+export async function getAllLogs(): Promise<LogEntry[]>
+{
+    try
+    {
+        const response = await fetch(API_BASE_URL);
+        if (!response.ok) 
+        {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+
+        const data: LogEntry[] = await response.json();
+        return data;
+    }
+    catch (error)
+    {
+        console.error(`Failed to fetch Domains: ${error}`)
+        return [];
+    }    
+}
+
+export async function getALogById(id: number): Promise<LogEntry | null> 
+{
+    try 
+    {
+        const response = await fetch(`${API_BASE_URL}/${id}`);
+        if(!response.ok) 
+        {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data: LogEntry = await response.json();
+        return data;
+    }
+    catch(error)
+    {
+        console.error(`Failed to get Domain by Id ${id}: ${error}`);
+        return null;
+    }
+}
