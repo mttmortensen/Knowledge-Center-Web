d9a536735a87e45bcb9fee0d66b7245337fb4664
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-06 14:53:33 -0600

    linking create page to main ui

--- ts/ui/kn/knowledge-node-main-ui.ts
+++ ts/ui/kn/knowledge-node-main-ui.ts
@@ -2,13 +2,20 @@ document.addEventListener("DOMContentLoaded", () =>
 {
     // === Main Menu Button Logic ===
     const viewBtn = document.getElementById("view-nodes");
+    const crtBtn = document.getElementById("create-node");
 
-    if (viewBtn) 
+    if (viewBtn && crtBtn) 
     {
         viewBtn.addEventListener("click", async () => 
         {
             window.location.href = "/kn/knowledge-nodes-list.html"
         })
+
+        crtBtn.addEventListener("click", async () => 
+        {
+            window.location.href = "/kn/knowledge-nodes-create.html"
+        })
+
     }
 
     // === Back button for Main Menu ===
