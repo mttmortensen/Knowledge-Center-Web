bb4750f9c3ebb9aed7fbc2d601bee92db99d27e5
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-06 14:32:43 -0600

    starting on tags now. got main menu and simple logic going for it

---
+++ ts/ui/tags/tags-main-ui.ts
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => 
+{
+    // === Main Menu Button Logic ===
+    const viewBtn = document.getElementById("view-tagss");
+
+    if (viewBtn) 
+    {
+        viewBtn.addEventListener("click", async () => 
+        {
+            window.location.href = "/tags/tags-list.html"
+        })
+    }
+
+    // === Back button for Main Menu ===
+    const backBtn = document.getElementById("back-button");
+    if(backBtn)
+    {
+        backBtn.addEventListener("click", () => 
+        {
+            window.history.back();
+        });
+    }
+    
+})
