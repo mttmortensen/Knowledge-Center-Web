46238e4d838fc667cffc7d3f04d2be792a28d38f
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-06 14:43:42 -0600

    tags list page and logic done

---
+++ ts/ui/tags/tags-list-ui.ts
@@ -0,0 +1,43 @@
+import { getAllTags } from "../../services/tag-service.js";
+
+document.addEventListener("DOMContentLoaded", () => 
+{
+    // === Tags List Page Logic ===
+    const tagListContainer = document.getElementById("tag-list");
+    if(tagListContainer) 
+    {
+        loadAndRenderTagList(tagListContainer);
+    }
+
+    // === Back button for Tag List Page ===
+    const backBtn = document.getElementById("back-button");
+    if(backBtn)
+    {
+        backBtn.addEventListener("click", () => 
+        {
+            window.history.back();
+        });
+    }
+});
+
+async function loadAndRenderTagList(container: HTMLElement) 
+{
+    const tags = await getAllTags();
+
+    if (tags.length == 0) 
+    {
+        container.textContent = "No Tags found.";
+        return;
+    }
+
+    tags.forEach(tag => 
+    {
+        const li = document.createElement("li");
+        
+        const p = document.createElement("p");
+        p.textContent = `${tag.Name}`;
+
+        li.appendChild(p);
+        container.appendChild(li);
+    });
+}
