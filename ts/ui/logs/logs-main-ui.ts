e0cdd0e224805a5dbeae0b6a125dbd17200e6396
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-07 20:25:22 -0600

    linking into log main

--- ts/ui/logs/logs-main-ui.ts
+++ ts/ui/logs/logs-main-ui.ts
@@ -2,12 +2,18 @@ document.addEventListener("DOMContentLoaded", () =>
 {
     // === Logs Menu Button Logic ===
     const viewBtn = document.getElementById("view-logs");
+    const crtBtn = document.getElementById("create-log");
 
-    if (viewBtn) 
+    if (viewBtn && crtBtn) 
     {
         viewBtn.addEventListener("click", async () => 
         {
             window.location.href = "/logs/logs-list.html"
+        })
+
+        crtBtn.addEventListener("click", () => 
+        {
+            window.location.href = "/logs/logs-create.html"
         })
     }
 
