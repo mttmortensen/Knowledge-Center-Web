31c44caf4355a993373c101735964cc64b11c682
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-08 10:19:36 -0600

    logic for update list. rn just a back btn

---
+++ ts/ui/kn/knowledge-node-list-update-ui.ts
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", async () => 
+{
+    // === Back button ===
+    const backBtn = document.getElementById("back-button");
+    if(backBtn)
+    {
+        backBtn.addEventListener("click", () => 
+        {
+            window.history.back();
+        });
+    }
+})
