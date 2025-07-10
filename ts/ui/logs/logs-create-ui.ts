cd600b0b7f1d9accbc2f585b8579227009b6bd7a
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-10 14:50:58 -0600

    Needed to remove entry data  and update Contributes to be a bool for validateField() condition

--- ts/ui/logs/logs-create-ui.ts
+++ ts/ui/logs/logs-create-ui.ts
@@ -24,13 +24,12 @@ document.addEventListener("DOMContentLoaded", () =>
             const Content = formData.get("content") as string;
             const TagId = parseInt(formData.get("tagId") as string);
             const ContributesToProgress = formData.get("contributesToProgress") === "true";
-            const EntryDate = new Date().toISOString(); 
             
             // === ✅ Validation ===
             if 
             (
                 !validateField(Content, { label: "Content", required: true, minLength: 3, maxLength: 2000 }) ||
-                !validateField("ContributesToProgress", { label: "Contribute to Progress", required: true,})
+                !validateField(ContributesToProgress, { label: "Contribute to Progress", required: true,})
             ) 
             {
                 console.error("❌ One or more validation checks failed.");
@@ -42,8 +41,7 @@ document.addEventListener("DOMContentLoaded", () =>
                 NodeId,
                 Content,
                 TagId,
-                ContributesToProgress,
-                EntryDate
+                ContributesToProgress
             }
 
             const success = await createLog(newLog);
