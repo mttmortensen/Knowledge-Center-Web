02517fa8e45a0e77fcb06679fe50be5e22ea75bb
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-10 11:24:03 -0600

    updating url

--- ts/ui/partials/global-nav-ui.ts
+++ ts/ui/partials/global-nav-ui.ts
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       if (token) {
         try {
-          await fetch("https://api.mortensens.xyz/kc/api/logout", {
+          await fetch("https://api.mortensens.xyz/kc/api/auth/logout", {
             method: "POST",
             headers: {
               "Authorization": `Bearer ${token}`
