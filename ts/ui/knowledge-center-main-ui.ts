41ff67a666eb57d64ab75461148d239b956ff01f
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-10 12:14:47 -0600

    producing the correct html pages based on live server path

--- ts/ui/knowledge-center-main-ui.ts
+++ ts/ui/knowledge-center-main-ui.ts
@@ -3,25 +3,25 @@ import { requireAuth } from "../services/auth-check.js";
 requireAuth();
 
 document.getElementById("quick-log")?.addEventListener("click", () => {
-  window.location.href= "/logs/logs-create.html"
+  window.location.href= "/html/logs/logs-create.html"
 })
 
 document.getElementById("quick-create-node")?.addEventListener("click", () => {
-  window.location.href = "/kn/knowledge-nodes-create.html"
+  window.location.href = "/html/kn/knowledge-nodes-create.html"
 })
 
 document.getElementById("menu-nodes")?.addEventListener("click", () => {
-  window.location.href = "/kn/knowledge-nodes-main.html";
+  window.location.href = "/html/kn/knowledge-nodes-main.html";
 });
 
 document.getElementById("menu-domains")?.addEventListener("click", () => {
-  window.location.href = "/domains/domains-main.html";
+  window.location.href = "/html/domains/domains-main.html";
 });
 
 document.getElementById("menu-logs")?.addEventListener("click", () => {
-  window.location.href = "/logs/logs-main.html";
+  window.location.href = "/html/logs/logs-main.html";
 });
 
 document.getElementById("menu-tags")?.addEventListener("click", () => {
-  window.location.href = "/tags/tags-main.html";
+  window.location.href = "/html/tags/tags-main.html";
 });
