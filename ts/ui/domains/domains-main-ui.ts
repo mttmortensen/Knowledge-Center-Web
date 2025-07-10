57a2c0e62a0fd9a880d59980434bf975236fa0f6
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-10 12:14:58 -0600

    adding /html/ to match live server hosting

--- ts/ui/domains/domains-main-ui.ts
+++ ts/ui/domains/domains-main-ui.ts
@@ -5,10 +5,10 @@ requireAuth();
 document.addEventListener("DOMContentLoaded", () => 
 {
     // === Main Menu Button Logic ===
-    setupDomainNav("view-domains", "/domains/domains-list.html")
-    setupDomainNav("create-domain", "/domains/domains-create.html")
-    setupDomainNav("update-domain", "/domains/domains-list-update.html")
-    setupDomainNav("delete-domain", "/domains/domains-list-delete.html")
+    setupDomainNav("view-domains", "/html/domains/domains-list.html")
+    setupDomainNav("create-domain", "/html/domains/domains-create.html")
+    setupDomainNav("update-domain", "/html/domains/domains-list-update.html")
+    setupDomainNav("delete-domain", "/html/domains/domains-list-delete.html")
 
 })
 
