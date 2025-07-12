8b75d565a5d5cf5b4d84b4ba8b4b56028bae9357
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-12 00:01:11 -0600

    adding of the demo button/feature

--- ts/services/login-service.ts
+++ ts/services/login-service.ts
@@ -1,11 +1,15 @@
 import { authFetch } from "./fetch-wrapper.js";
 
 const loginForm = document.getElementById("login-form") as HTMLFormElement;
+const demoLoginButton = document.getElementById("demo-login-btn") as HTMLButtonElement;
 const errorMessage = document.getElementById("error-message") as HTMLParagraphElement;
 
 const API_BASE_URL = "https://api.mortensens.xyz/kc/api";
 
-loginForm.addEventListener("submit", async (event) => {
+
+// This is to handle when a user hits that login button
+loginForm.addEventListener("submit", async (event) => 
+{
   event.preventDefault(); // Stop form from refreshing the page
 
   const username = (document.getElementById("username") as HTMLInputElement).value;
@@ -33,5 +37,36 @@ loginForm.addEventListener("submit", async (event) => {
   } catch (err) {
     console.error("Login failed:", err);
     errorMessage.textContent = "Something went wrong. Please try again.";
+  }
+});
+
+// This is to handle when a user hits the demo button 
+demoLoginButton.addEventListener("click", async () => 
+{
+  try 
+  {
+    const response = await authFetch(`${API_BASE_URL}/auth/demo`, 
+    {
+      method: "POST"
+    });
+    
+    if (response.ok) 
+    {
+      const data = await response.json();
+      sessionStorage.setItem("token", data.token);
+      sessionStorage.setItem("isDemo", "true");
+      
+      // Redirect to main page
+      window.location.href = "/index.html"
+    }
+    else 
+    {
+      errorMessage.textContent = "Failed to start demo mode.";      
+    }
+  }
+  catch (err) 
+  {
+    console.error("Demo login failed: ", err);
+    errorMessage.textContent = "Something went wrong when entering demo mode.";
   }
 });
